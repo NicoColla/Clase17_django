4ca88ce84d20c5a98df85c1419e4bc35e196a6d1
Nicolas Colla Cameron <ns.colla@gmail.com>
2024-05-11 15:33:41 -0300

    improve: views > probando_templates

--- Proyecto/config/views.py
+++ Proyecto/config/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.template import Context, Template
+from django.shortcuts import render
+from datetime import datetime
 
 
 def saludo(request):
@@ -13,10 +15,16 @@ def nombre(request, nombre: str, apellido: str):
     apellido = apellido.capitalize()
     return HttpResponse(f"<h1>{apellido}, {nombre}</h1>")
 
+#def probando_template(request):
+#    mi_html = open("./templates/template1.html", encoding="UTF-8")
+#    mi_template = Template(mi_html.read())
+#    mi_html.close()
+#    mi_contexto = Context({"Saludo": "HOLA TU COLA XD", "Autor": "Lenga"})
+#    mi_documento = mi_template.render(mi_contexto)
+#    return HttpResponse(mi_documento)
+
 def probando_template(request):
-    mi_html = open("./templates/template1.html", encoding="UTF-8")
-    mi_template = Template(mi_html.read())
-    mi_html.close()
-    mi_contexto = Context({"Saludo": "HOLA TU COLA XD", "Autor": "Lenga"})
-    mi_documento = mi_template.render(mi_contexto)
-    return HttpResponse(mi_documento)
+    datos = {"saludo": "HOLA TU COLA JAJA XD", 
+             "autor": "Nicolás Uriel Lenga",
+             "date": datetime.now()}
+    return render(request, "template1.html", datos)
